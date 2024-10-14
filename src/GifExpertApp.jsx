53a1c1551a1d02282fc7b4cc9c587f227dba96ab
Ezequiel Magi <ezequielmaggi@gmail.com>
2024-10-13 21:40:22 -0300

    agrego nuevo elemento a la lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,20 +1,30 @@
-
+import { useState } from 'react'
 
 export const GifExpertApp = () => {
 
-    const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+    const [ Categories , SetCategories ] = useState(['One Punch', 'Samurai X', 'Hunter X Hunter'])
+
+    const onAddCategory = (e) => {
+        e.preventDefault()
+
+
+        SetCategories(['Naruto' , ...Categories])
+    }
 
   return (
     <div className="bg-slate-400 h-screen p-8">
         <h1>Gif Expert App</h1>
 
-        <input
-            type="text"
-            placeholder="Search your gif here..." 
-        />
+        <form className='p-4' onSubmit={onAddCategory}>
+            <input
+                type="text"
+                placeholder="Search your gif here..." 
+            />
+            <button className='m-4 p-2 bg-orange-50'>Agregar</button>
+        </form>
 
         {
-            categories.map(category => {
+            Categories.map(category => {
                 return <li key={category}>{category}</li>
             })    
         }
